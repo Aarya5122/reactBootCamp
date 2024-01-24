e1e3e04f316ac690aeb5ae06236d81df76334d25
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2024-01-24 22:32:55 +0530

    Multiple elements in React

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const h1Element = React.createElement("h1", {}, "Hello World");
+const h1Element = React.createElement(
+	"h1",
+	{ key: "heading" },
+	"Om Namo Venkateshaya!"
+);
+const paraElement = React.createElement("p", { key: "para" }, "Hello World");
 
-root.render(h1Element);
+const divElement = React.createElement("div", { className: "root" }, [
+	h1Element,
+	paraElement,
+]);
+
+console.log("para element object: ", paraElement);
+console.log("div element object: ", divElement);
+
+root.render(divElement);
